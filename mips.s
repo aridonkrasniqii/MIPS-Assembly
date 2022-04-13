50d373228b3d9f8fef55ea44a0713260d4ccf77f
Aridon Krasniqi <77694113+aridonkrasniqii@users.noreply.github.com>
2022-04-13 16:08:47 +0200

    Update mips.s

--- mips.s
+++ mips.s
@@ -16,22 +16,22 @@ largest:                                 # function largest
 move $s0 , $a0                           # first arr address
 lw $t8 , 4($s0)                          #  max = array[0]
 move $t6 , $a1                           # int n
-
-addi $t1, $t1, 0                        # int  i = 1
+ 
+addi $t1, $t1, 0                         # int  i = 1
 
         loop:
 
             slti $t2 , $t1 , 6           # if $t1 < 5 ? $t2 = 1 : $t2 = 0
             beq $t2, $zero , exit        # if $t2 = 0 jump to exit
 
-            lw $t0 , 0($s0)               # loads member of array
+            lw $t0 , 0($s0)              # loads member of array
 
-            addi $s0 , $s0 , 4            # increment $s0 to go to next member of array
-            addi $t1, $t1, 1               # i ++
+            addi $s0 , $s0 , 4           # increment $s0 to go to next member of array
+            addi $t1, $t1, 1             # i ++
 
-            slt $t9 , $t0 , $t8            # $ t0 < $t8 ? $t9 = 1 : $t9 = 0
-            bne $t9 , $zero ,loop          # if $t9 != 0 jump to loop
-            move $t8 , $t0                 # max = arr[i]
+            slt $t9 , $t0 , $t8          # $ t0 < $t8 ? $t9 = 1 : $t9 = 0
+            bne $t9 , $zero ,loop        # if $t9 != 0 jump to loop
+            move $t8 , $t0               # max = arr[i]
 
         j loop
 
