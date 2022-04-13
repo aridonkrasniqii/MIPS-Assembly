5ef3a6fe5fa0ce9aaa219fc8b553762163a7ecb7
Aridon Krasniqi <77694113+aridonkrasniqii@users.noreply.github.com>
2022-04-13 16:08:01 +0200

    Update mips.s

--- mips.s
+++ mips.s
@@ -4,59 +4,38 @@
 
 main:
 
-# get first address of array
-la $a0 , array
+la $a0 , array                           # first arr address
+la $a1 , 6                               # initialize n = 6
 
+jal largest                              # jump and load address to function
 
-# initialize n = 5
-la $a1 , 6
+j finalexit                              # when function is returned jump to finalexit
 
+largest:                                 # function largest
 
+move $s0 , $a0                           # first arr address
+lw $t8 , 4($s0)                          #  max = array[0]
+move $t6 , $a1                           # int n
 
-jal largest
-
-j finalexit
-
-
-
-largest:
-
-move $s0 , $a0
-lw $t8 , 4($s0) # storing max = array[0]
-move $t6 , $a1
-
-# int  i = 1
-addi $t1, $t1, 1
-
+addi $t1, $t1, 0                        # int  i = 1
 
         loop:
 
-            # if $t1 < 5 ? $t2 = 1 : $t2 = 0
-            slt $t2 , $t1 , $t6
-            beq $t2, $zero , exit
+            slti $t2 , $t1 , 6           # if $t1 < 5 ? $t2 = 1 : $t2 = 0
+            beq $t2, $zero , exit        # if $t2 = 0 jump to exit
 
+            lw $t0 , 0($s0)               # loads member of array
 
+            addi $s0 , $s0 , 4            # increment $s0 to go to next member of array
+            addi $t1, $t1, 1               # i ++
 
-            # get member of array
-            lw $t0 , 0($s0)
+            slt $t9 , $t0 , $t8            # $ t0 < $t8 ? $t9 = 1 : $t9 = 0
+            bne $t9 , $zero ,loop          # if $t9 != 0 jump to loop
+            move $t8 , $t0                 # max = arr[i]
 
-            # increment $s0 to go to other member of array
-            addi $s0 , $s0 , 4
-
-
-            # $ t0 < $t8 ? $t9 = 1 : $t9 = 0
-            # $t0 = arr[i] , max = $t8
-            # if (arr[i] > max ) max = arr[i]
-            slt $t9 , $t0 , $t8
-            bne $t9 , $zero ,loop
-            move $t8 , $t0
-
-             #   i = i + 1
-            addi $t1, $t1, 1
         j loop
 
         exit:
-       
 
 jr $ra
 
@@ -70,8 +49,7 @@ finalexit:
     move $a0 , $t8
     syscall
 
-
 .data
 
-array: .word 2, 1, 3, 4, 5, 232
+array: .word 2222, 1, 53, 444, 5, 232
 str: .asciiz "Largest in given array is: "
